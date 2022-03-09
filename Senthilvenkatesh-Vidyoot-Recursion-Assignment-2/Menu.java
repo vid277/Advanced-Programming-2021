0d6d4c35397f03565aa440a23d1567ea6d487ade
Vidyoot Senthilvenkatesh <vidyoots@gmail.com>
2022-03-09 00:08:32 -0600

    Minor Fixes with error checking

--- Senthilvenkatesh-Vidyoot-Recursion-Assignment-2/Menu.java
+++ Senthilvenkatesh-Vidyoot-Recursion-Assignment-2/Menu.java
@@ -1,17 +1,34 @@
+/**
+ * This is the main menu class that displays the options to the user and allows the user to pick from a variety of options
+ *
+ * @author Vidyoot Senthilvenkatesh
+ * @version 3/8/2022
+ */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Menu {
-    public static final int[] arraySorted = {-56, 0, 2, 5, 6, 12, 22, 31, 43, 49, 89, 111};
-    public static final int[] arrayUnsorted = {12, 5, -56, 43, 22, 31, 2, 6, 0, 49, 111, 89};
 
+    //Constants that contain unsorted and sorted lists of integers
+    public static final int[] ARRAY_SORTED = {-56, 0, 2, 5, 6, 12, 22, 31, 43, 49, 89, 111};
+    public static final int[] ARRAY_UNSORTED = {12, 5, -56, 43, 22, 31, 2, 6, 0, 49, 111, 89};
+
+    /**
+     * Calls the menuPane method, which contains the options to display to the user.
+     */
     public Menu(){
         System.out.println("______________________________________________________________");
 
         menuPane();
     }
 
+    /**
+     * This is the main menu method that is used to prompt the user with options to choose from
+     * Calls the methods based on the user input
+     * Also performs some error checking and input validity testing on the user input
+     */
     public void menuPane(){
         methodOptions options = new methodOptions();
         Scanner scanner = new Scanner(System.in);
@@ -19,6 +36,7 @@ public class Menu {
         System.out.println("Would you like to have fun with recursion? yes/no: ");
         String userInput = scanner.nextLine();
 
+        //Options to display to the user
         if ((userInput.equalsIgnoreCase("yes"))){
             System.out.println("______________________________________________________________");
             System.out.println("Please select one of the following: ");
@@ -31,14 +49,20 @@ public class Menu {
 
             userInput = scanner.nextLine();
 
+            //This entire block checks the user input and process the user input
+            //Transmits the user input to the appropriate methods
             if (userInput.equals("1")){
                 System.out.println("How many terms would you like in the sequence? ");
                 userInput = scanner.nextLine();
 
                 if (checkIfValid(userInput)){
-                    System.out.println("The final value is: " + options.Fibonacci(Integer.parseInt(userInput)));
+                    System.out.printf("Fibonacci series upto %s numbers : \n", userInput);
+                    for(int i = 1; i <= Integer.parseInt(userInput); i++) {
+                        System.out.print(options.Fibonacci(i) + " ");
+                    }
+                    System.out.println();
                 }
-
+                menuPane();
             }
 
             else if(userInput.equals("2")){
@@ -50,25 +74,28 @@ public class Menu {
                 else {
                     if(checkIfValid(userInput)){
                         System.out.println(options.moveDisks(Integer.parseInt(userInput), 1, 2, 3));
-                        System.out.printf("It took %d moves for this Towers of Hanoi problem", options.getNumberOfTries());
+                        System.out.printf("It took %d moves for this Towers of Hanoi problem \n", options.getNumberOfTries());
                     }
                 }
+                menuPane();
             }
 
             else if(userInput.equals("3")){
-                System.out.println("Here is the sorted int array:" + Arrays.toString(arraySorted));
+                System.out.println("Here is the sorted int array:" + Arrays.toString(ARRAY_SORTED));
                 System.out.println("Give me a number you would like to search for in the list: ");
 
                 userInput = scanner.nextLine();
 
                 if (checkIfValid(userInput)){
-                    if  (options.binarySearch(arraySorted, 0, arraySorted.length-1, Integer.parseInt(userInput)) == -1){
+                    if  (options.binarySearch(ARRAY_SORTED, 0, ARRAY_SORTED.length-1, Integer.parseInt(userInput)) == -1){
                         System.out.println("The number is not present in the list");
                     }
                     else {
-                        System.out.println("The index is: " + options.binarySearch(arraySorted, 0, arraySorted.length-1, Integer.parseInt(userInput)));
+                        System.out.printf("%s was found at index %d \n", userInput, options.binarySearch(ARRAY_SORTED, 0, ARRAY_SORTED.length-1, Integer.parseInt(userInput)));
                     }
                 }
+
+                menuPane();
             }
 
             else if(userInput.equals("4")){
@@ -79,15 +106,15 @@ public class Menu {
 
 
                 if (checkIfValid(num1) && checkIfValid(num2)){
-                    if(Integer.parseInt(num1) >= arrayUnsorted.length || Integer.parseInt(num2) >= arrayUnsorted.length) {
+                    if(Integer.parseInt(num1) >= ARRAY_UNSORTED.length || Integer.parseInt(num2) >= ARRAY_UNSORTED.length) {
                         System.out.println("Error, Invalid Input");
                         menuPane();
                     }
                     else{
-                        System.out.println(options.rangeSum(arrayUnsorted, Integer.parseInt(num1), Integer.parseInt(num2)+1));
+                        System.out.println(options.rangeSum(ARRAY_UNSORTED, Integer.parseInt(num1), Integer.parseInt(num2)+1));
                     }
                 }
-
+                    menuPane();
                 }
 
             else {
@@ -104,24 +131,42 @@ public class Menu {
         }
     }
 
+    /**
+     * This is used by all the options to ensure that the user options do not contain letters
+     *
+     * @param userInput - The string input by the user
+     * @return boolean - true if the user inputs only digits, false otherwise
+     */
     public boolean checkIfValid(String userInput){
 
         boolean isValid = false;
 
-        for (int i = 0; i < userInput.length(); i++){
-            if (Character.isDigit(userInput.charAt(i))){
-                isValid = true;
-            }
-            else{
-                isValid = false;
-                System.out.println("Invalid Input");
-                menuPane();
-                break;
+        //Used to account for the possibility of a negative number
+        if (userInput.startsWith("-")){
+            isValid = true;
+        }
+        else{
+            for (int i = 0; i < userInput.length(); i++){
+                if (Character.isDigit(userInput.charAt(i))){
+                    isValid = true;
+                }
+                else{
+                    isValid = false;
+                    System.out.println("Invalid Input");
+                    menuPane();
+                    break;
+                }
             }
         }
 
         return isValid;
     }
+
+    /**
+     * This is the main method, and it is used to create an object of the Menu class
+     *
+     * @param args
+     */
     public static void main(String[] args){
         new Menu();
     }
--- Senthilvenkatesh-Vidyoot-Recursion-Assignment-2/methodOptions.java
+++ Senthilvenkatesh-Vidyoot-Recursion-Assignment-2/methodOptions.java
@@ -1,11 +1,21 @@
+/**
+ * This is the methodOptions class that contains the methods for all possible options for the user to choose from
+ * These are all recursion based algorithms
+ *
+ * @author Vidyoot Senthilvenkatesh
+ * @version 3/8/2022
+ */
 public class methodOptions {
+
+    //Required variables to store important information for certain methods
     private int sum;
     private int numberOfTries;
 
-    public methodOptions() {
-
-    }
-
+    /**
+     * Used to calculate the fibonacci sequence until a certain number
+     * @param n - the nth term of the fibonacci sequence
+     * @return - the value of a certain term of th fibonacci sequence
+     */
     public int Fibonacci(int n){
         if (n <= 1){
             return n;
@@ -13,10 +23,20 @@ public class methodOptions {
         return Fibonacci(n-1) + Fibonacci(n-2);
     }
 
+    /**
+     * Used to calculate the number of moves required to complete the tower of hanoi problems
+     * Utilizes recursion
+     *
+     * @param num - the number of disks
+     * @param fromPeg - The peg to move a disk from (Peg A)
+     * @param toPeg - The peg to move a disk to (Peg B)
+     * @param TempPeg - The temporary peg to move a disk for short term storage (Peg C)
+     * @return - the peg to move the disks to and from
+     */
     public int moveDisks(int num, int fromPeg, int toPeg, int TempPeg){
         if (num == 1)
         {
-            System.out.println("Move a disk from " +  fromPeg + " to peg " + toPeg);
+            System.out.println("Move a disk from peg " +  fromPeg + " to peg " + toPeg);
             return numberOfTries + 1;
         }
         numberOfTries += 2;
@@ -25,10 +45,24 @@ public class methodOptions {
         return moveDisks(num-1, TempPeg, toPeg, fromPeg);
     }
 
+    /**
+     * Gets the number of tries required for the tower of hanoi problems
+     *
+     * @return - the number of tries required for the tower of hanoi problem
+     */
     public int getNumberOfTries() {
         return numberOfTries + 1;
     }
 
+    /**
+     * This is a binary search algorithm that finds the index of a certain element in a sorted array
+     *
+     * @param array - a sorted array containing integers
+     * @param first - the first index of the array
+     * @param last - the last index of the array
+     * @param value - the value to search for
+     * @return - the index of the element in the array, -1 if the element is not found in the array, or a call to the binarySearch method.
+     */
     public int binarySearch(int[] array, int first, int last, int value) {
             if (last >= first) {
                 int middle = first + (last - first) / 2;
@@ -43,7 +77,14 @@ public class methodOptions {
             return -1;
     }
 
-
+    /**
+     * This finds the  sum of elements from the start index to the end index in the array
+     *
+     * @param array - the array of elements
+     * @param start - the index to start summing from
+     * @param end - the index to end summing from
+     * @return - the sum of the elements between the start index to the end index of the array
+     */
     public int rangeSum(int[] array, int start, int end){
         if (end < start){
             System.out.println("The second index has to be lower than the first index");
